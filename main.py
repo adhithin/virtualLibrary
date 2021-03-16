a323ef4b33ae1f9b8560e6d2e18b84585f6fd73d
adhithin <adhithi.nmurthy07@gmail.com>
2021-03-15 23:57:23 -0700

    set up main.py & home.html

---
+++ main.py
@@ -0,0 +1,28 @@
+import os
+from flask import Flask, render_template, flash, redirect, url_for, session, logging
+from flask import request
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy import desc
+from bs4 import BeautifulSoup
+import smtplib
+import time
+
+app = Flask(__name__)
+
+basedir = os.path.abspath(os.path.dirname(__file__))
+
+# Connects our Flask App to our Database
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, 'data.sqlite')
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+#OMG SO IMPORTANT TO INCLUDE THIS ABOVE! Warnings up the wazoo if not here on a development server.
+
+db = SQLAlchemy(app)
+
+@app.route('/')
+def home():
+    return render_template("home.html")
+
+if __name__ == "__main__":
+    #runs the application on the repl development server
+    app.run(debug=True, port='8242', host='127.0.0.1')
+
