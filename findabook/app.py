b2be573647be49ca2ad00bbab19abe7835fd43f7
aditiakella <aditi.s.akella@gmail.com>
2021-03-25 11:14:53 -0700

    added a commit towards minilab

--- findabook/app.py
+++ findabook/app.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request
 
 bookfinder_bp = Blueprint('findabook', __name__,
                          template_folder='templates',
