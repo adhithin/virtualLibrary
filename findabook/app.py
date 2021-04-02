a943e600587a3f426aed8974301ac39f6d46fec5
aditiakella <aditi.s.akella@gmail.com>
2021-04-01 20:16:20 -0700

    Algo MiniLab

--- findabook/app.py
+++ findabook/app.py
@@ -1,4 +1,5 @@
 from flask import Blueprint, render_template, request
+from findabook.algorithm import Pascal
 
 bookfinder_bp = Blueprint('findabook', __name__,
                          template_folder='templates',
@@ -8,3 +9,9 @@ bookfinder_bp = Blueprint('findabook', __name__,
 @bookfinder_bp.route('/')
 def aditihome():
     return render_template("step1.html")
+
+@bookfinder_bp.route('/algo', methods=["GET", "POST"])
+def fibonacci():
+    if request.form:
+        return render_template("algorithm.html", pascal=Pascal(int(request.form.get("series"))))
+    return render_template("algorithm.html", pascal=Pascal(2))
