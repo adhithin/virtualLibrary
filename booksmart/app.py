723f7514ba0cef70fd13fb0fa86f9eff6d0ec9e6
dayita-ray <dayitaray@gmail.com>
2021-04-02 21:54:29 -0700

    dayita minilab

---
+++ booksmart/app.py
@@ -0,0 +1,27 @@
+
+from flask import Flask, Blueprint, render_template
+from booksmart.rat import Rats
+
+
+booksmart_bp = Blueprint('booksmart', __name__,
+                          template_folder='templates',
+                          static_folder='static', static_url_path='assets')
+
+@booksmart_bp.route('/', methods=['GET', 'POST'])
+def home():
+    if request.method == 'POST':
+        a = int(request.form.get("series"))
+        #function = Rats(a/a)
+        return render_template("swag.html", function=Rats(a))
+    return render_template("swag.html", function=Rats(1))
+
+import os
+from flask import Flask, Blueprint, render_template, flash, redirect, url_for, session, logging
+from flask import request
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy import desc
+#from bs4 import BeautifulSoup
+import smtplib
+import time
+
+
