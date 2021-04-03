559fec75186ab05a32561506ea5ac203ee1c6aef
lukemanning244 <72890051+lukemanning244@users.noreply.github.com>
2021-04-02 23:26:32 -0700

    mini lab

--- main.py
+++ main.py
@@ -10,7 +10,8 @@ from emails.app import emails_bp
 from findabook.app import bookfinder_bp
 from booksearch.app import booksearch_bp
 from booksmart.app import booksmart_bp
-from blueprint.luke.routes import luke
+from MiniLab.app import MiniLab_bp
+
 
 
 app = Flask(__name__)
@@ -18,7 +19,7 @@ app.register_blueprint(emails_bp, url_prefix='/emails')
 app.register_blueprint(bookfinder_bp, url_prefix='/findabook')
 app.register_blueprint(booksearch_bp, url_prefix='/booksearch')
 app.register_blueprint(booksmart_bp, url_prefix='/booksmart')
-app.register_blueprint(luke, url_prefix='/Mini Lab')
+app.register_blueprint(MiniLab_bp, url_prefix='/MiniLab')
 
 basedir = os.path.abspath(os.path.dirname(__file__))
 
