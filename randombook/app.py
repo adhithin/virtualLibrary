f309b840e1f348b916fde3047501fc5d22d32d2f
krispykreme14 <72890029+krispykreme14@users.noreply.github.com>
2021-04-02 23:40:16 -0700

    Ketki Minilab: added book.html

--- randombook/app.py
+++ randombook/app.py
@@ -6,5 +6,22 @@ from sqlalchemy import desc
 #from bs4 import BeautifulSoup
 import smtplib
 import time
+from booksearch.selection import Books
+
+
+booksearch_bp = Blueprint('randombook', __name__,
+                          template_folder='templates',
+                          static_folder='static', static_url_path='assets')
+
+
+@randombook_bp.route('/', methods=['GET', 'POST'])
+def search():
+    if request.method == 'POST':
+        return render_template("book.html", authorrecs=Authors(int(request.form.get("series"))))
+    return render_template("book.html", bookrecs=Authors(1))
+
+@randombook_bp.route('/allgenres', methods=['GET', 'POST'])
+def allgenres():
+    return render_template("allgenres.html")
 
 
