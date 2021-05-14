a6197bd48cf5d8ba6ff39ea89dadbad50ea27590
lukemanning244 <72890051+lukemanning244@users.noreply.github.com>
2021-05-14 15:38:22 -0700

    mini lab

--- lukebp/bp.py
+++ lukebp/bp.py
@@ -1,3 +1,4 @@
+import requests
 from flask import Blueprint, render_template, request, render_template_string
 from lukebp.bubblesort import bubblesort, bubble
 from lukebp.squared import Squared
@@ -31,3 +32,22 @@ def sortform(dtype=None):
         data = newdata
 
         return render_template_string('Output of Sorted Data: ' + " ".join(data))
+
+@squared_bp.route('/twitter', methods=['GET'])
+def twitter():
+    url = "https://twitter32.p.rapidapi.com/getProfile"
+
+    querystring = {"username":"bookofthemonth"}
+
+    headers = {
+        'x-rapidapi-key': "0fe3a85372mshec4ebae6a3667a4p169becjsn644301488221",
+        'x-rapidapi-host': "twitter32.p.rapidapi.com"
+    }
+
+    response = requests.request("GET", url, headers=headers, params=querystring)
+    name = response.json().get('screen_name')
+    lastupdate = response.json().get('lastupdate')
+    profiles = response.json().get('profiles')
+    followers = response.json().get('followers')
+    print(response.text)
+    return render_template('tweet.html', response=response, name=name, profiles=profiles, lastupdate=lastupdate, followers=followers)
