23978deae26f30d621820feb6a35a5d62fcfc64b
aditiakella <aditi.s.akella@gmail.com>
2021-05-20 10:48:05 -0700

    Jigger Tickets

--- findabook/app.py
+++ findabook/app.py
@@ -18,18 +18,18 @@ def pascalalgo():
     return render_template("algorithm.html", pascal=Pascal(2))
 
 @bookfinder_bp.route('/bubblesort', methods=['GET', 'POST'])
-def sortform():
+def sort():
     if request.method == 'GET':
-        return render_template("bubblesort.html")
+        return render_template("bubblesort.html") # fetches the list from the user interface
     elif request.method == 'POST':
-        userlist = request.form.get('inputintegers').split()
-        newlist=[]
+        userlist = request.form.get('inputintegers').split() # the variable userlist is assigned to the string of numbers that the user inputs
+        newlist=[] # an empty list called newlist is created
         for element in userlist:
-            newlist.append(int(element))
-        userlist = newlist
-        BubbleSort(userlist)
-        newlist = []
+            newlist.append(int(element)) # every number from userlist is converted from a string to an integer and added to the end of newlist
+        userlist = newlist # userlist is redefined as the list numbers that the user inputs
+        BubbleSort(userlist) # userlist is passed through the procedure BubbleSort as a parameter, which sorts userlist
+        newlist = [] # userlist is redefined as an empty list
         for element in userlist:
-            newlist.append(str(element))
-        userlist = newlist
-        return render_template_string('Experimental Data Sorted from Least to Greatest Value: ' + " ".join(userlist))
+            newlist.append(str(element)) # every sorted number from userlist is converted from an integer to a string and added to the end of newlist
+        userlist = newlist # userlist is redefined as the list numbers that the user inputs
+        return render_template_string('Experimental Data Sorted from Least to Greatest Value: ' + " ".join(userlist)) # prints "Experimental Data Sorted from Least to Greatest Value:" and prints the sorted list of data afterwards
--- findabook/bubblesort.py
+++ findabook/bubblesort.py
@@ -9,10 +9,10 @@ def BubbleSort(list):
         list = list
 
 
-int_list = [64]
+int_list = [64] # This is a sample list used to test to test the code under "else" in the program
 
-BubbleSort(int_list)
+BubbleSort(int_list) # int_list is passed as a parameter through the procedure BubbleSort
 
-print ("Sorted list is:")
+print ("Sorted list is:") # The phrase "Sorted list is" is displayed
 for i in range(len(int_list)):
-    print ("%d" %int_list[i])
+    print ("%d" %int_list[i]) # prints the list of sorted numbers
