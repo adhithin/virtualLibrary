3e998bf5257ca93d25bfa22eecb6481d03a0b23e
adhithin <adhithi.nmurthy07@gmail.com>
2021-05-29 14:00:34 -0700

    added in reviews page

--- main.py
+++ main.py
@@ -51,6 +51,25 @@ class Ebook(db.Model):
         return f"{self.p_title},{self.p_price}"
 
 db.create_all();
+
+class Rating(db.Model):
+    __tablename__ = 'ratings'
+    id = db.Column(db.Integer, primary_key=True)
+    # not planning to delete scores, but still a good practice
+    p_book = db.Column(db.String(10), unique=False, nullable=False)
+    p_rating = db.Column(db.Integer, unique=False, nullable=False) # want score as int so we can sort by it easily.
+    p_commment = db.Column(db.String(10), unique=False, nullable=False)
+
+    def __init__(self, p_book, p_rating, p_commment):
+        self.p_book = p_book
+        self.p_rating = p_rating
+        self.p_commment = p_commment
+
+    def __repr__(self):
+        return f"{self.p_book},{self.p_rating}, {self.p_commment}"
+
+#must go after 'models'
+db.create_all();
 db = SQLAlchemy(app)
 
 
@@ -87,6 +106,30 @@ def home():
 
     return render_template("home.html")
 
+@app.route('/bookreviews', methods=['GET', 'POST'])
+def review():
+    if request.method == 'POST':
+        book = request.form['book']
+        rating = int(request.form['rating'])
+        comment = request.form['comment']
+        #the code below confirmed I had the proper data. Now to add it to the db.
+        print(book)
+        print(rating)
+        print(comment)
+
+        new_review = Rating(book, rating, comment)
+        db.session.add(new_review)
+        db.session.commit()
+
+    #query the db for the ratings:
+    results = Rating.query.order_by(desc('p_rating')).all()
+    arcade_ratings = []
+
+    for result in results:
+        rating_dict = {'book':result.p_book, 'rating':result.p_rating, 'comment':result.p_commment}
+        arcade_ratings.append(rating_dict)
+
+    return render_template('bookreview.html', arcade_ratings = arcade_ratings)
 
 
 if __name__ == "__main__":
